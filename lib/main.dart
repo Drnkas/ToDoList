4aad86cf15845ca0ebcc762718cc35bff9d38750
Drnkas <belaisad@hotmail.com>
2024-04-06 00:56:15 -0300

    finished app

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:todo_list/src/home/controller/home_controller.dart';
-import 'package:todo_list/src/splash/splash_page.dart';
+import 'package:todo_list/src/home/view/home_page.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         useMaterial3: true,
       ),
-      home: SplashPage(),
+      home: const HomePage(),
     );
   }
 }
